a28b257bdf5577be3b1cdd118a8791c47ac11a9b
Adam Veldhousen <adamveld12@gmail.com>
2014-11-29 10:04:44 -0600

    add room structure and item structure

--- Source/DungeonGenerator/Navigation/ITileMap.cs
+++ Source/DungeonGenerator/Navigation/ITileMap.cs
@@ -3,7 +3,24 @@
     public interface ITileMap
     {
         ushort this[int x, int y] { get; set; }
+
         int Height { get; }
         int Width { get; }
+    }
+
+    public struct Room
+    {
+        public int X;
+        public int Y;
+
+        public int Width;
+        public int Height;
+    }
+
+
+    public struct Item
+    {
+        public Point Location;
+        public byte Type;
     }
 }
