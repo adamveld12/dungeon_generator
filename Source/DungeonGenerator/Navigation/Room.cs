7299cc1987fec4be49f0be966c07a641839ceb48
Adam Veldhousen <adamveld12@gmail.com>
2014-11-30 14:13:40 -0600

    create room and item cs files. Move relevant code to them

---
+++ Source/DungeonGenerator/Navigation/Room.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Dungeon.Generator.Navigation
+{
+    public struct Pathway
+    {
+        public Room Start;
+        public Room End;
+    }
+
+    public struct Room
+    {
+        public int X;
+        public int Y;
+
+        public int Width;
+        public int Height;
+
+        /// <summary>
+        /// Sets the X/Y coordinate of the item to inside of this room
+        /// </summary>
+        /// <param name="item">The item to place</param>
+        public Item PlaceItem(Item item)
+        {
+            var oldxy = item.Location;
+            item.Location = new Point
+            {
+                X = X + (oldxy.X % Width),
+                Y = Y + (oldxy.Y % Height)
+            };
+            return item;
+        }
+
+        public Point GetCenterWallPoint(Direction direction)
+        {
+            int x = X;
+            int y = Y;
+            switch (direction)
+            {
+                case Direction.N:
+                    x += Width/2;
+                    break;
+                case Direction.E:
+                    y += Height/2;
+                    x += Width;
+                    break;
+                case Direction.S:
+                    y += Height;
+                    x += Width/2;
+                    break;
+                case Direction.W:
+                    y += Height/2;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("direction");
+            }
+
+            return new Point
+            {
+                Direction = direction, 
+                X = x,
+                Y = y,
+            };
+        }
+    }
+}
